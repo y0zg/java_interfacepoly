fa4753126cd9e608b12c088e8d4ef38d2ef85aac
michmauzer004 <michmauzer004@gmail.com>
2016-05-15 20:21:53 +0300

    SalaryEmployee + getPayable from interface

--- SalariedEmployee.java
+++ SalariedEmployee.java
@@ -1,5 +1,34 @@
 /**
  * Created by webstddesign on 12.05.2016.
  */
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee {
+	public double weeklySalary;
+
+	public SalariedEmployee(String firstName, String lastName, String securityNumber, double weeklySalary) {
+		super(firstName, lastName, securityNumber);
+
+		if (weeklySalary < 0) {
+			throw new IllegalArgumentException("Weekly salary must be > 0");
+		}
+
+		this.weeklySalary = weeklySalary;
+	}
+
+	public void setWeeklySalary(double weeklySalary){
+		this.weeklySalary=weeklySalary;
+	}
+
+public String toString(){
+	return 	String.format("%s: %n%s, %s %n%s: %s",
+			"SalariedEmployee First & Last name",
+			getFirstName(),
+			getLastName(),
+			"SecurityNumber",
+			getSecurityNumber()
+	);
+}
+
+	public double getPayable(){
+		return weeklySalary;
+	}
 }
