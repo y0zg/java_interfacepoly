6cb4bfdda3a484a91db3fc9f2c2c4d6b08c22ff6
michmauzer004 <michmauzer004@gmail.com>
2016-05-15 20:23:28 +0300

    SalaryEmployee + getPayable from interface

--- Employee.java
+++ Employee.java
@@ -1,5 +1,30 @@
 /**
  * Created by webstddesign on 12.05.2016.
  */
-public class Employee {
+public abstract class Employee implements Payable{
+	public String firstName;
+	public String lastName;
+	public String securityNumber;
+
+	public Employee(String firstName, String lastName, String securityNumber) {
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.securityNumber = securityNumber;
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getSecurityNumber() {
+		return securityNumber;
+	}
+
+
 }
